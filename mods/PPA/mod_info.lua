ea037bf389cf71bba1b14bd6cb2d82ec71b561b3
4z0t <gastec139@gmail.com>
2022-09-17 17:00:46 +0300

    PPA: Version 2

--- mods/PPA/mod_info.lua
+++ mods/PPA/mod_info.lua
@@ -1,8 +1,8 @@
 name = "Pause Plz AFK"
-uid = "pause-plz-afk-4z0t-v01"
-version = 1
+uid = "pause-plz-afk-4z0t-v02"
+version = 2
 copyright = ""
-description = [[If you accidentally missed match in tmm mod will pause game for you and send message for others about your AFK state.
+description = [[If you accidentally missed match in TMM mod will pause game for you and send message for others about your AFK state.
 You can edit message in message.lua in mod folder]]
 author = "4z0t"
 url = ""
