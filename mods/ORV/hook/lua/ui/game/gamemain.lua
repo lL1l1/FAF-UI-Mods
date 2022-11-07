84bed035d9352141462853c7e81337d10b4bdb42
4z0t <gastec139@gmail.com>
2022-11-07 13:57:42 +0300

    remove dependency

--- mods/ORV/hook/lua/ui/game/gamemain.lua
+++ mods/ORV/hook/lua/ui/game/gamemain.lua
@@ -1,6 +1,8 @@
-local OldCreateUI = CreateUI
-function CreateUI(isReplay)
-    OldCreateUI(isReplay)
-    import('/mods/ORV/modules/Main.lua').Main(isReplay)
-    import("/lua/ui/game/reclaim.lua").SetMapSize()
+if import("/lua/version.lua").GetVersion() >= 3745 then
+    local OldCreateUI = CreateUI
+    function CreateUI(isReplay)
+        OldCreateUI(isReplay)
+        --import('/mods/ORV/modules/Main.lua').Main(isReplay)
+        import("/lua/ui/game/reclaim.lua").SetMapSize()
+    end
 end
