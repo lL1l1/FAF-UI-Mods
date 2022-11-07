5cfdacac4dd0ff4f9bbff6d04b23a4595b7e24c4
4z0t <gastec139@gmail.com>
2022-11-07 13:59:27 +0300

    ORV Version 3

--- mods/ORV/mod_info.lua
+++ mods/ORV/mod_info.lua
@@ -1,11 +1,13 @@
 name = "Optimized Reclaim View"
-version = 2
+version = 3
 copyright = ""
 description = [[No more 0 FPS when reclaim is displayed!
-Significantly changes the process of reclaim labels being displayed to reduce FPS drops due to memory reallocations]]
+Significantly changes the process of reclaim labels being displayed to reduce FPS drops due to memory reallocations.
+Since last update provides only classic coloring of reclaim labels.
+Color is based on biggest prop in group.]]
 author = "4z0t"
 url = ""
-uid = "optimized-reclaim-view-v02"
+uid = "optimized-reclaim-view-v03"
 icon = "/mods/ORV/icon.png"
 selectable = true
 enabled = true
