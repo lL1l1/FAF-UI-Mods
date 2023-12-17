ec9f4dd2f0986c5c378ddb7181ef4b3faf885441
4z0t <gastec139@gmail.com>
2023-12-17 14:57:39 +0300

    init Advanced Key Actions

---
+++ mods/AKA/Main.lua
@@ -0,0 +1,7 @@
+local KeyMapper = import("/lua/keymap/keymapper.lua")
+
+
+function Main()
+
+
+end
---
+++ mods/AKA/mod_info.lua
@@ -0,0 +1,11 @@
+name = "Advanced Key Actions"
+version = 1
+copyright = ""
+description = [[]]
+author = "4z0t"
+url = ""
+uid = "advanced-key-actions-v01"
+exclusive = false
+ui_only = true
+conflicts = {}
+__umt = true
