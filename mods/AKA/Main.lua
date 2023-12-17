c0d5dc37e9e8803142dfc9db558ca10e6c36cac2
4z0t <gastec139@gmail.com>
2023-12-17 15:19:50 +0300

    annotate

--- mods/AKA/Main.lua
+++ mods/AKA/Main.lua
@@ -15,18 +15,56 @@ CategoryMatcher = Class()
     end
 }
 
+---@alias Action string|fun(selection:UserUnit[])
+
 ---@class CategoryAction
+---@field _actions Action[]
+---@field _category EntityCategory
 CategoryAction = Class()
 {
+    ---@param self any
+    ---@param category any
     __init = function(self, category)
-
+        self._actions = {}
+        self._category = category
     end,
 
+    ---comment
+    ---@param self CategoryAction
+    ---@param action any
     Action = function(self, action)
-
+        table.insert(self._actions, action)
     end,
 
+    ---comment
+    ---@param self CategoryAction
     Match = function(self, selection)
+
+        return false
+    end,
+
+    ---comment
+    ---@param self CategoryAction
+    Process = function(self, selection)
+        if self:Match(selection) then
+            self:Execute(selection)
+            return true
+        end
+        return false
+    end,
+
+    ---comment
+    ---@param self CategoryAction
+    Execute = function(self, selection)
+        for _, action in self._actions do
+            if type(action) == "string" then
+                ConExecute(action)
+            elseif type(action) == "function" then
+                action(selection)
+            else
+                error()
+            end
+        end
     end
 }
 
@@ -41,7 +79,7 @@ local actions =
             :Action "StartCommandMode order RULEUCC_Transport",
         CategoryAction(categories.COMMAND + categories.SUBCOMMANDER)
             :Action "UI_Lua import('/lua/ui/game/orders.lua').EnterOverchargeMode()",
-    }
+    },
 }
 
 
