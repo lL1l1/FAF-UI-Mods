5c27df9543c06860fd81336c2e41ad12d64fd7dc
4z0t <gastec139@gmail.com>
2024-11-19 20:45:37 +0300

    add icon

--- mods/RFA/mod_info.lua
+++ mods/RFA/mod_info.lua
@@ -4,10 +4,12 @@ version = 1
 copyright = ""
 description = [[
 Shows radiuses of weapons of selected units and build range. Also shows them with unit under mouse.
+See options for mod in UI mods options menu!
 
 Requires UI mod tools!
 ]]
 author = "4z0t"
+icon = "/mods/RFA/icon.png"
 url = ""
 selectable = true
 enabled = true
